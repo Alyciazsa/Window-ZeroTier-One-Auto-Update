0345c6e4fccf6ac34ecf8167087bc2f61369f3de
Alyciazsa <97645837+Alyciazsa@users.noreply.github.com>
2023-06-27 17:20:22 +0700

    Update Zerotier AutoUpdate.bat

--- Zerotier AutoUpdate.bat
+++ Zerotier AutoUpdate.bat
@@ -1,6 +1,5 @@
-curl "https://download.zerotier.com/dist/ZeroTierOne.msi" -o "C:\ProgramData\ZeroTier\ZeroTierOne.msi"
-msiexec /i "C:\ProgramData\ZeroTier\ZeroTierOne.msi" /qn
-del "C:\ProgramData\ZeroTier\ZeroTierOne.msi"
-net stop "ZeroTierOneService"
-net start "ZeroTierOneService"
+#install ZeroTierOne by Winget
+winget install ZeroTier.ZeroTierOne
+#Restart Service
+sc stop "ZeroTierOneService" && sc start "ZeroTierOneService"
 exit
